98e39fd61b8edab738a4449153968aeb96df2562
Diogo Lacerda <diogo.lacerda@gmail.com>
2016-06-23 01:09:44 -0300

    database configurations for heroku

--- config/database.php
+++ config/database.php
@@ -54,11 +54,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'ec2-54-243-190-37.compute-1.amazonaws.com'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'd5rv0900h23imf'),
-            'username' => env('DB_USERNAME', 'oflojbcapcqpxr'),
-            'password' => env('DB_PASSWORD', 'zk4tqaDbPlhTv49W4sfAKP7jWn'),
+            'host' => env('DB_HOST', 'localhost'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
@@ -68,11 +68,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => env('DB_HOST', 'ec2-54-243-190-37.compute-1.amazonaws.com'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'd5rv0900h23imf'),
+            'username' => env('DB_USERNAME', 'oflojbcapcqpxr'),
+            'password' => env('DB_PASSWORD', 'zk4tqaDbPlhTv49W4sfAKP7jWn'),
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
